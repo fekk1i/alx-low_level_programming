37c46e6d2a916d7f5abf3c7202d3d361f0dabbf6
fekk1i <alielfekki@gmail.com>
2023-09-08 01:52:01 -0700

    123

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main = Entry point
+ *
+ * return: Always 0 (Success)
+ */
+int main(void)
+{
+	char prg = "\"Programming is like building a multilingual puzzle";
+
+	puts(prg);
+	return (0);
+}
