bcf99f1bea6efa76456d08aa1ebb23d147a7738e
fekk1i <alielfekki@gmail.com>
2023-09-11 14:15:28 -0700

    123

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int digit;
+
+	for (digit = 0; digit < 10; digit++)
+	{
+		putchar('0' + digit);
+
+	if (digit < 9)
+	{
+		putchar(',');
+		putchar(' ');
+	}
+}
+
+	putchar('\n');
+
+	return (0);
+}
