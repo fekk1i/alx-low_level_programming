4f53e10691d92aa2a4e01fdb541f467bf83128ce
fekk1i <alielfekki@gmail.com>
2023-09-14 06:17:36 -0700

    123

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * add - Entry point
+ *
+ * @a: value 1
+ * @b: value 2
+ *
+ * Return: Always 0 (Success)
+ */
+
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+	printf("%d", c);
+}
