9ca2bcc2b6873bf4d48553c2b99c828e7b7edb23
fekk1i <alielfekki@gmail.com>
2023-09-14 05:53:43 -0700

    123

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * jack_bauer - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+void jack_bauer(void);
+{
+	int hour;
+	int minute;
+	char hour_str;
+	char minute_str;
+
+	for (hour = 0; hour < 24; hour++)
+	{
+		for (minute = 0; minute < 60; minute++)
+		{
+			char hour_str[3], minute_str[3];
+
+			snprintf(hour_str, sizeof(hour_str), "%02d", hour;
+			snprintf(minute_str, sizeof(minute_str), "%02d", minute;
+
+			printf("%s:%s", hour_str, minute_str);
+		}
+	}
+
+}
