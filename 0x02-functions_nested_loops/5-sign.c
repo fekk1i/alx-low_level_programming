fcae7a3c386ab2cf540776be2d1136dbcc61639a
fekk1i <alielfekki@gmail.com>
2023-09-14 04:49:50 -0700

    123

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,16 +11,15 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		putchar('+');
-		return (1);
 	}
 	else if (n == 0)
 	{
 		putchar('0');
-		return (0);
 	}
 	else
 	{
 		putchar('-');
-		return (-1);
 	}
+
+	return ((n > 0) ? 1 : (n == 0) ? 0 : -1);
 }
