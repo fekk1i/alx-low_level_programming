3bccca27dfe5d59b21d667fa70ea46ed212f5145
fekk1i <alielfekki@gmail.com>
2023-09-14 02:43:02 -0700

    123

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+	char *putchar_str = "_putchar\n";
+
+	printf("%c"), putchar_str;
+
+	return (0);
+}
