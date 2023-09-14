750609706a3ada96b1ff1dbf983f505b4561a297
fekk1i <alielfekki@gmail.com>
2023-09-14 04:59:59 -0700

    123

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * print_sign - Entry point
@@ -10,17 +11,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		putchar('+');
+		_putchar('+');
 		return (1);
 	}
 	else if (n == 0)
 	{
-		putchar(48);
+		_putchar(48);
 		return (0);
 	}
 	else
 	{
-		putchar('-');
+		_putchar('-');
 	}
 
 	return (-1);
