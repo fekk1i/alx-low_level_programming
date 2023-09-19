10d1defe457d4ec4faa0b239420874ede87520d8
fekk1i <alielfekki@gmail.com>
2023-09-19 10:46:29 -0700

    123

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -8,11 +8,12 @@
  */
 void print_numbers(void)
 {
-int i;
+	int i;
 
-for (i = 0; i < 10; i++)
-{
-_putchar('0' + i);
-}
-_putchar('\n');
+	for (i = '0'; i <= '9'; i++)
+	{
+		_putchar(i);
+	}
+
+	_putchar('\n');
 }
